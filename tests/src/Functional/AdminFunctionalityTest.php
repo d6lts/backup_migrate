6a7261ff714574394543a3e2508c114b9f13c6e3
Damien McKenna <damien@mc-kenna.com>
2020-09-28 08:21:27 -0400

    Issue #3158790 by devad, raman.b, karolus, robcarr, DamienMcKenna, endless_wander, Bagz, jhnnsbstnbch, abdul_azeez_drusys, celia_ccs: Unable to add schedules and profiles.

--- tests/src/Functional/AdminFunctionalityTest.php
+++ tests/src/Functional/AdminFunctionalityTest.php
@@ -84,6 +84,8 @@ class AdminFunctionalityTest extends BrowserTestBase {
       ['admin/config/development/backup_migrate/schedule/add', 'Add schedule'],
       ['admin/config/development/backup_migrate/settings', 'Settings'],
       ['admin/config/development/backup_migrate/settings/add', 'Add settings profile'],
+      ['admin/config/development/backup_migrate/settings/destination', 'Backup Destination'],
+      ['admin/config/development/backup_migrate/settings/destination/add', 'Add destination'],
       ['admin/config/development/backup_migrate/settings/source', 'Backup sources'],
       ['admin/config/development/backup_migrate/settings/source/add', 'Add Backup Source'],
     ];
@@ -141,6 +143,128 @@ class AdminFunctionalityTest extends BrowserTestBase {
     $session->pageTextContains('Saved Test destination.');
     $session->pageTextContains('Private Files Directory');
     $session->pageTextContains('private_files');
+  }
+
+  /**
+   * Make sure the 'schedules' system works correctly.
+   */
+  public function testSchedulesAdmin() {
+    // Load the schedule page.
+    $this->drupalGet('admin/config/development/backup_migrate/schedule');
+    $session = $this->assertSession();
+    $session->statusCodeEquals(200);
+    $session->pageTextContains('Schedule Name');
+    $session->pageTextContains('Enabled');
+    $session->pageTextContains('Frequency');
+    $session->pageTextContains('Last Run');
+    $session->pageTextContains('Next Run');
+    $session->pageTextContains('Keep');
+    $session->pageTextContains('Daily Schedule');
+    $session->pageTextContains('No');
+    $session->pageTextContains('Every 0 weeks');
+    $session->pageTextContains('Never');
+    $session->pageTextContains('Disabled');
+    $session->pageTextContains('Last backups');
+    // @todo Confirm the table only has one record.
+
+    // Load the schedule-add form.
+    $this->drupalGet('admin/config/development/backup_migrate/schedule/add');
+    $session = $this->assertSession();
+    $session->statusCodeEquals(200);
+    $session->pageTextContains('Add schedule');
+    $session->fieldExists('label');
+    $session->fieldExists('id');
+    $session->fieldExists('enabled');
+    $session->fieldExists('source_id');
+    $session->fieldExists('destination_id');
+    $session->fieldExists('period_number');
+    $session->fieldExists('period_type');
+    $session->fieldExists('keep');
+    $session->buttonExists('Save');
+
+    // Create a new schedule.
+    $edit = [
+      'label' => 'Test schedule',
+      'id' => 'test_schedule',
+      'enabled' => TRUE,
+      'source_id' => 'default_db',
+      'destination_id' => 'private_files',
+      'period_number' => 14400,
+      'period_type' => 'minutes',
+      'keep' => 1000,
+    ];
+    $this->drupalPostForm(NULL, $edit, 'Save');
+
+    $session = $this->assertSession();
+    $session->statusCodeEquals(200);
+    $session->addressEquals('admin/config/development/backup_migrate/schedule');
+    $session->pageTextContains('Created the Test schedule Schedule.');
+    $session->pageTextContains('Every 10 days');
+    $session->pageTextContains('Last 1000 backups');
+    $session->pageTextContains('Yes');
+  }
+
+  /**
+   * Make sure the 'profiles' system works correctly.
+   */
+  public function testProfilesAdmin() {
+    // Load the profiles page.
+    $this->drupalGet('admin/config/development/backup_migrate/settings');
+    $session = $this->assertSession();
+    $session->statusCodeEquals(200);
+    $session->pageTextContains('Settings');
+    $session->pageTextContains('Profile Name');
+    // @todo Confirm the table has no records.
+
+    // Load the profile-add form.
+    $this->drupalGet('admin/config/development/backup_migrate/settings/add');
+    $session = $this->assertSession();
+    $session->statusCodeEquals(200);
+    $session->pageTextContains('Add settings profile');
+    $session->fieldExists('label');
+    $session->fieldExists('id');
+    $session->fieldExists('config[namer][filename]');
+    $session->fieldExists('config[namer][timestamp]');
+    $session->fieldExists('config[namer][timestamp_format]');
+    $session->fieldExists('config[compressor][compression]');
+    $session->fieldExists('config[utils][site_offline]');
+    $session->fieldExists('config[metadata][description]');
+    $session->fieldExists('config[db_exclude][exclude_tables][]');
+    $session->fieldExists('config[db_exclude][nodata_tables][]');
+    $session->fieldExists('config[private_files_exclude][exclude_filepaths]');
+    $session->fieldExists('config[public_files_exclude][exclude_filepaths]');
+    $session->buttonExists('Save');
+
+    // Create a new profile.
+    $edit = [
+      'label' => 'Test profile',
+      'id' => 'test_profile',
+      'config[namer][filename]' => 'test_backup',
+      'config[namer][timestamp]' => 'Y-m-d\TH-i-s',
+      'config[compressor][compression]' => 'none',
+      'config[utils][site_offline]' => 1,
+      'config[metadata][description]' => 'Test description text.',
+      'config[db_exclude][exclude_tables][]' => [],
+      'config[db_exclude][nodata_tables][]' => [],
+      'config[private_files_exclude][exclude_filepaths]' => 'test_private_exclude',
+      'config[public_files_exclude][exclude_filepaths]' => 'test_public_exclude',
+    ];
+    $this->drupalPostForm(NULL, $edit, 'Save');
+
+    $session = $this->assertSession();
+    $session->statusCodeEquals(200);
+    $session->addressEquals('admin/config/development/backup_migrate/settings');
+    $session->pageTextContains('Created the Test profile Settings Profile.');
+    $session->pageTextContains('Profile Name');
+    $session->pageTextContains('Machine name');
+    $session->pageTextContains('Compression');
+    $session->pageTextContains('Take site offline');
+    $session->pageTextContains('Description');
+    $session->pageTextContains('Test profile');
+    $session->pageTextContains('test_profile');
+    $session->pageTextContains('none');
+    $session->pageTextContains('1');
+    $session->pageTextContains('Test description text.');
   }
 
 }
